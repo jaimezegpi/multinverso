5f015ec21a2606424738702060c5f8f0cb397dcc
jzegpi <jzegpi@mtm.cl>
2018-09-11 05:29:54 -0300

    3d Canvas added

--- action.php
+++ action.php
@@ -6,7 +6,6 @@ time
 0 position
 1 rotation
 */
-
 /*------------USER DATA----------- INI*/
 if ( isset( $_GET['user_data'] ) ){
 	$user_data = $_GET['user_data'];
@@ -19,6 +18,7 @@ if ( isset( $_GET['user_data'] ) ){
 if( !isset( $_SESSION['player_id'] ) ){
 	$_SESSION['player_id'] = session_id();
 }
+
 
 if ( !file_exists('server_population.dat') ){
 	$server_population = fopen( "server_population.dat", "w" );
@@ -42,17 +42,26 @@ if ( count($server_population_clients)<1 || $server_population=='' ){
 	$user_detector = false;
 	foreach ( $server_population_clients as $key => $client ) {
 		$client_data = explode(";", $client);
-		if (isset($client_data[1])){
-			$client_pause_time = $t-$client_data[1];
-			echo $client_pause_time;
+
+		if ( isset( $client_data[0] ) ){ $client_data_player_id = $client_data[0]; }else{
+			$client_data_player_id = ''; continue;
+		}
+
+		if ( isset( $client_data[1] ) ){ $client_data_player_time = $client_data[1]; }else{
+			$client_data_player_time = '';
+			continue;
+		}
+
+		if (isset($client_data_player_time)){
+			$client_pause_time = $t-$client_data_player_time;
 			if ($client_pause_time>50){ continue; }
 		}else{ continue; }
 
-		if ( $key>0 && isset($client_data[1]) ){
+		if ( $key>0 ){
 			$return_data.="|";
 		}
 
-		if ($_SESSION['player_id'] == $client_data[0]){
+		if ($_SESSION['player_id'] == $client_data_player_id){
 			$user_detector = true;
 			$return_data.=$_SESSION['player_id'].";".$t.";".$user_data;
 		}else{
@@ -60,8 +69,13 @@ if ( count($server_population_clients)<1 || $server_population=='' ){
 		}
 
 	}
-	if (!$user_detector && isset($client_data[1]) ){
-		$return_data.="|".$_SESSION['player_id'].";".$t.";".$user_data;
+	if (!$user_detector && isset($client_data_player_time) ){
+		if ($return_data){
+			$return_data.="|".$_SESSION['player_id'].";".$t.";".$user_data;
+		}else{
+			$return_data.=$_SESSION['player_id'].";".$t.";".$user_data;
+		}
+		
 	}
 	$server_population = fopen( "server_population.dat", "w+" );
 	fwrite($server_population, $return_data);
@@ -71,4 +85,5 @@ if ( count($server_population_clients)<1 || $server_population=='' ){
 	exit();
 }
 
+echo $_SESSION['player_id'];
 exit();
